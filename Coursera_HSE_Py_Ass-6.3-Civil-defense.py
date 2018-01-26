7af6f3826c4dd1fb4e59edeb912bb909f21d9d1c
Yuri Revin <yuri.revin@gmail.com>
2018-01-26 12:58:05 +1100

    Ver.1.1 Test 5 Wrong answer!

--- Coursera_HSE_Py_Ass-6.3-Civil-defense.py
+++ Coursera_HSE_Py_Ass-6.3-Civil-defense.py
@@ -1,32 +1,36 @@
 n = int(input())
-a = map(int, input().split())
+ln = list(map(int, input().split()))
 m = int(input())
-b = list(map(int, input().split()))
+lm = list(map(int, input().split()))
+lnn = []
+lmm = []
+to = []
+o = []
+oo = []
 
-for i in range(len(b)):
-    b[i] = [i + 1, b[i]]
+for i in range(0, n):
+    t = (i + 1, ln[i])
+    lnn.append(t)
+    lnn.sort()
 
-b.sort(key=lambda x: x[1])
+for i in range(0, m):
+    t = (i + 1, lm[i])
+    lmm.sort()
+    lmm.append(t)
 
-
-def find_value(x):
-    if (x < b[0][1]):
-        return b[0][0]
-    if (x > b[-1][1]):
-        return b[-1][0]
-    l = 0
-    r = len(b) - 1
-    # b[l][1] < x
-    while (r - l > 1):
-        m = (r + l) >> 1
-        if (b[m][1] < x):
-            l = m
+for i in range(n):
+    for j in range(m):
+        if lnn[i][1] - lmm[j][1] >= 0:
+            to.append(lnn[i][1] - lmm[j][1])
         else:
-            r = m
-    if (x - b[l][1] < b[r][1] - x):
-        return b[l][0]
-    else:
-        return b[r][0]
+            to.append(-1 * (lnn[i][1] - lmm[j][1]))
 
+for i in range(0, len(to), m):
+    too = tuple(to[i:i+m])
+    o.append(too)
 
-print(*[find_value(v) for v in a])
+for i in range(len(o)):
+    j = o[i]
+    oo.append(j.index(min(j)) + 1)
+
+print(' '.join(map(str, oo)))
